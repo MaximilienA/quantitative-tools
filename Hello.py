81a84fe0f5b2b7a39599872c1445ceb560647d36
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-11-27 23:21:43 +0000

    commit hello

--- Hello.py
+++ Hello.py
@@ -26,7 +26,7 @@ def run():
 
     st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a demo aboveedeeeeeeeeeeeeeeeeeee.")
 
     st.markdown(
         """
