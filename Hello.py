237cee547a62640ec2f4e4d6fadf18f33291eeff
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-11-30 23:08:13 +0000

    C

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlitttttt! 👋")
+    st.write("# Welcome to Quantitative tools!")
 
     st.sidebar.success("Select a demo abo.")
 
@@ -34,15 +34,6 @@ def run():
         Machine Learning and Data Science projects.
         **👈 Select a demo from the sidebar** to see some examples
         of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
 
