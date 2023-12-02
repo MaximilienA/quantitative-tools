75bafa97d7c0122b20158c72fcd61f62aa122a17
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-02 18:33:54 +0000

    C

--- pages/Correlation calculator.py
+++ pages/Correlation calculator.py
@@ -12,7 +12,7 @@ import pages.database
 # import plotly.express as px
 
 # number_of_days = 150
-pages.database.insertdata("Connected 3")
+
 #DATA FRAME FOR MATCHING INDEX NAME WITH INDEX SYMBOL
 data  = {
     'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
@@ -264,3 +264,5 @@ fig.update_layout(xaxis_rangeslider_visible=True)
 # st.pyplot(plt)
 st.plotly_chart(fig)
 
+pages.database.insertdata(selected_name)
+pages.database.insertdata(datetime.date.today())
