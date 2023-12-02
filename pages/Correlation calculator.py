507d45c274c6f95834324e999887f110b2d023ad
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-02 17:59:47 +0000

    C

--- pages/Correlation calculator.py
+++ pages/Correlation calculator.py
@@ -7,7 +7,7 @@ import yfinance as yf
 import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
-import Database
+import database
 # import plotly.express as px
 
 # number_of_days = 150
