cd609023b96bf2b74686fa14702a5f7dc00388dd
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-02 16:17:55 +0000

    C

--- Hello.py
+++ Hello.py
@@ -20,7 +20,7 @@ from firebase_admin import db
 from firebase_admin import auth
 
 cred = credentials.Certificate('quantitativetoolsdatabase-90b51ea9a1ca.json')
-firebase_admin.initialize(cred, {"databaseURL" : "https://quantitativetoolsdatabase-default-rtdb.europe-west1.firebasedatabase.app/"})
+firebase_admin.initialize_app(cred, {"databaseURL" : "https://quantitativetoolsdatabase-default-rtdb.europe-west1.firebasedatabase.app/"})
 
 ref = db.reference("/")
 
