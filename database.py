78d830c17f6cbecf6c4bcceb70339523dc7be26f
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-02 18:25:35 +0000

    C

--- database.py
+++ database.py
@@ -12,7 +12,8 @@ ref = db.reference("/Test")
 # st.write(ref.get()) 
 
 def insertdata(data):
-    # db.reference("/Test").push().set(data)
     st.write("TEEEST")
+    db.reference("/Test").push().set(data)
+    
 
 st.write(ref.get()) 
