56789664c47b51ad2c39f38fc67a5bd323d4f93b
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-02 18:27:51 +0000

    C

--- pages/Correlation calculator.py
+++ pages/Correlation calculator.py
@@ -7,11 +7,12 @@ import yfinance as yf
 import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
-# import database
+import database
+
 # import plotly.express as px
 
 # number_of_days = 150
-
+database.insertdata("Connected 3")
 #DATA FRAME FOR MATCHING INDEX NAME WITH INDEX SYMBOL
 data  = {
     'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
