e93fbc7223c705c205e4cc24446fd0e18592405e
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-03 19:39:33 +0000

    C

--- pages/Projected FED rates.py
+++ pages/Projected FED rates.py
@@ -310,7 +310,7 @@ now = datetime.datetime.now()
 now = now.replace(hour=now.hour + 1)
 formatted_time = now.strftime("%H:%M:%S")
 
-json_data = final_scrapped_df.to_json(orient='split')
+json_data = final_scrapped_df.to_json(orient='index')
 
 # # Convert JSON object to a list
 # data_list = list(json_data.items())
