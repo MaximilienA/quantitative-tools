d4e775fe1a0f6a3980aa13fd44b7a989f3e24670
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-03 18:01:23 +0000

    C

--- backend/automatedquery.py
+++ backend/automatedquery.py
@@ -1,15 +1,21 @@
 import datetime
 import time
 import backend.database
+import pandas as pd
 
 def lauchautomatedquery():
     i = 0
-    while i<10:
+    while i<5:
             now = datetime.datetime.now()
             now = now.replace(hour=now.houdr + 1)
             formatted_time = now.strftime("%H:%M:%S")
 
-            backend.database.insertdata(str(formatted_time))
+            #backend.database.insertdata(str(formatted_time))
             
-            time.sleep(5)
+            data = {'name': ['Alice', 'Bob', 'Charlie'], 'age': [32, 54, 27], 'city': ['New York', 'Chicago', 'Los Angeles']}
+            df = pd.DataFrame(data)
+            json_data = df.to_json(orient='records')
+            backend.database.insertdata(json_data)
+
+            time.sleep(3)
             i += 1
