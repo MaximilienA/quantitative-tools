dae9c2e1a9055f1200abb2c31ec2e59b5a6e12c3
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2023-12-05 22:20:40 +0000

    C

--- backend/projectedfedrates.py
+++ backend/projectedfedrates.py
@@ -135,7 +135,7 @@ def dataSlicer(data_headers_raw, data_probabilities_raw, index):
         percentages.insert(0, "0.0%")
         percentages.insert(0, "0.0%")  
         percentages.insert(0, "0.0%")
-        # percentages.insert(0, "0,0%")  
+        percentages.insert(0, "0.0%")  
     
     # Create a DataFrame
     df = pd.DataFrame([percentages], columns=data_headers_clean[1:])
