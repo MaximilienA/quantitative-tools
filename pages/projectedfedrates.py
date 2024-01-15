517e545bca8d45633b2a575d266b2cfdd309fb10
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2024-01-15 01:13:04 +0000

    Adding waiting message

--- pages/projectedfedrates.py
+++ pages/projectedfedrates.py
@@ -44,8 +44,6 @@ import backend.database
     # 7    07/11/2024 0,0% 0,0% 1,4% 13,9% 40,0% 32,3% 10...
     # 8    18/12/2024 1,1% 11,3% 34,4% 33,9% 15,2% 3,5% 0...
 def get_probabilities():
-    now = datetime.datetime.now()
-    print("Starting data scrapping at : ", now.strftime("%H:%M:%S"))
     options = Options()
     options.add_argument('--headless')
 
@@ -162,10 +160,13 @@ def dataSlicer(data_headers_raw, data_probabilities_raw, index):
     return df
 
 def dfRatesMerger():
-
+    now = datetime.datetime.now()
+    print("Starting data scrapping at : ", now.strftime("%H:%M:%S"))
+    st.write("Starting data scrapping")
     scrapped_data_from_website_df  = get_probabilities()
     now = datetime.datetime.now()
     print("Ending data scrapping at : ", now.strftime("%H:%M:%S"))
+    st.write("Data scrapping ended")
 
     Rates_df = pd.DataFrame({"Rates": ["0-25",	"25-50",	"50-75",	"75-100",	"100-125",	"125-150",	"150-175",	"175-200",	"200-225",	"225-250",	"250-275",	"275-300",	"300-325",	"325-350",	"350-375",	"375-400",	"400-425",	"425-450",	"450-475",	"475-500",	"500-525",	"525-550",	"550-575",	"575-600",	"600-625",	"625-650",	"650-675",	"675-700",	"700-725",	"725-750",	"750-775",	"775-800",	"800-825",	"825-850",	"850-875",	"875-900",	"900-925",	"925-950",	"950-975",	"975-1000"]})
 
@@ -258,15 +259,15 @@ def transformReworkedDataframeToDisplayableDataframe(dataframe_from_database):
 
     selected_columns = [dataframe_from_database.columns[0], dataframe_from_database.columns[1], dataframe_from_database.columns[2], dataframe_from_database.columns[3], dataframe_from_database.columns[4], dataframe_from_database.columns[5], dataframe_from_database.columns[6], dataframe_from_database.columns[7]]
 
-    print("selected_columns", selected_columns, "\n")
+    # print("selected_columns", selected_columns, "\n")
 
     max_values_probability_rate = dataframe_from_database[selected_columns].idxmax(axis=0)
 
-    print("max_values_indices", max_values_probability_rate, "\n")
+    # print("max_values_indices", max_values_probability_rate, "\n")
 
     max_probability = dataframe_from_database.max(axis=0)
 
-    print("max_values", max_probability, "\n")
+    # print("max_values", max_probability, "\n")
 
     # # Print the indices of the maximum values in each row
     # print("max_values_indices", max_values_indices)
