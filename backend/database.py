a5879f143b18df945136a2eaedf363cce0abfe0b
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2024-08-10 19:40:48 +0000

    Update gitignore

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ import datetime
 import utils
 import pandas as pd
 
-print("Hello", utils.getEnvValue('FIREBASE_PRIVATE_KEY'))
+print("Hello", utils.getEnvValue('FIREBASE_PROJECT_ID'))
 
 cred = credentials.Certificate( {
     "project_id": utils.getEnvValue('FIREBASE_PROJECT_ID'),
