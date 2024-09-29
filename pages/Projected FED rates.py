3037bfa20816faf66bd782ed477e178dfc1516d7
MaximilienA <90631665+MaximilienA@users.noreply.github.com>
2024-09-29 18:48:36 +0000

    Update graph type

--- pages/Projected FED rates.py
+++ pages/Projected FED rates.py
@@ -512,28 +512,28 @@ df_to_display_in_graph2 = transformReworkedDataframeToDisplayableDataframe(dataf
 # st.write(df_to_display_in_graph1)
 # st.write(df_to_display_in_graph2)
 
-# df_to_display_in_graph1
+df_to_display_in_graph1
 
-# # ===== PYPLOT =====
-# # Create the line graph
-# plt.figure(figsize=(16, 6))
-# plt.plot(df_to_display_in_graph1.index, df_to_display_in_graph1['Upper range rate'], marker='o', linestyle='-', label = date1)
-# plt.plot(df_to_display_in_graph2.index, df_to_display_in_graph2['Upper range rate'], marker='o', linestyle='-', label = date2)
-# plt.xlabel('Meeting dates')
-# plt.ylabel('Upper range rate')
-# plt.title('Projected FED rates for upcoming meeting dates')
-# plt.grid(True)
+# ===== PYPLOT =====
+# Create the line graph
+plt.figure(figsize=(16, 6))
+plt.plot(df_to_display_in_graph1.index, df_to_display_in_graph1['Upper range rate'], marker='o', linestyle='-', label = date1)
+plt.plot(df_to_display_in_graph2.index, df_to_display_in_graph2['Upper range rate'], marker='o', linestyle='-', label = date2)
+plt.xlabel('Meeting dates')
+plt.ylabel('Upper range rate')
+plt.title('Projected FED rates for upcoming meeting dates')
+plt.grid(True)
 
-# plt.legend()
+plt.legend()
 
-# # Scale Y axis by 0.25
-# def make_increment(start, end, num_steps):
-#     return [start + i * (end - start) / (num_steps - 1) for i in range(num_steps)]
+# Scale Y axis by 0.25
+def make_increment(start, end, num_steps):
+    return [start + i * (end - start) / (num_steps - 1) for i in range(num_steps)]
 
-# max_value = df_to_display_in_graph1['Upper range rate'].max()
-# min_value = df_to_display_in_graph1['Upper range rate'].min()
-# increment_values = make_increment(max_value+0.25, min_value-0.25, int((max_value-min_value)/0.25)+3)
-# plt.gca().set_yticks(increment_values)
+max_value = df_to_display_in_graph1['Upper range rate'].max()
+min_value = df_to_display_in_graph1['Upper range rate'].min()
+increment_values = make_increment(max_value+0.25, min_value-0.25, int((max_value-min_value)/0.25)+3)
+plt.gca().set_yticks(increment_values)
 
 # plt.show()
 
@@ -605,4 +605,4 @@ df2_reindexed = df_to_display_in_graph2.reindex(combined_dates)
 # Create the plot using Plotly
 # create_plot(df1_reindexed, df2_reindexed)
 
-st.pyplot(create_plot(df1_reindexed, df2_reindexed))
+st.plotly_chart(create_plot(df1_reindexed, df2_reindexed))
